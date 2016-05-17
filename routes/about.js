caec598d735ba4001c73364b1984c47c1e2b0c91
Serdar Güzel <admin@serdarguzel.com>
2016-05-17 04:52:24 +0300

    adding files

---
+++ routes/about.js
@@ -0,0 +1,4 @@
+/* GET home page. */
+exports.index = function(req, res){
+  res.render('about', { title: '11111', post : 'mypost' });
+};
---
+++ routes/contact.js
@@ -0,0 +1,4 @@
+/* GET contact page. */
+exports.index = function(req, res){
+  res.render('contact', { title: '11111', post : 'mypost' });
+};
